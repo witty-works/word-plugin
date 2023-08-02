f1f69aed2bbcbbc2854b0dfb86fed699dea280ff
Lukas Kahwe Smith <lukas.smith@witty.works>
2023-08-02 16:47:10 +0200

    bump version

--- src/sentry.ts
+++ src/sentry.ts
@@ -7,7 +7,7 @@ import { AppModule } from "./app/app.module";
 if (environment.sentry_dsn) {
   Sentry.init({
     environment: environment.environment,
-    release: `${environment.sentry_package_name}@1.0.6`,
+    release: `${environment.sentry_package_name}@1.0.7`,
     dsn: environment.sentry_dsn,
     integrations: [
     new Sentry.BrowserTracing({
