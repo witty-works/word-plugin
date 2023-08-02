f74448eb21a1cd10fa6c18aa4b35711e9b3ec28d
Lukas Kahwe Smith <lukas.smith@witty.works>
2023-08-02 17:20:56 +0200

    set sentry dsn for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   environment: 'production',
-  sentry_dsn: "",
+  sentry_dsn: "https://1b99be5ef9f8413614d3183638b22663@o512991.ingest.sentry.io/4505624728502272",
   sentry_api_url: "https://word.witty.works",
   sentry_sample_rate: 0.1,
   sentry_traces_sample_rate: 0.001, 
