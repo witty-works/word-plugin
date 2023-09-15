2c3e3be027c0d7aab973ed8fa6b5b260a7ecb6e7
Solveig Helland <45122311+hellasol@users.noreply.github.com>
2023-09-15 15:56:24 +0200

    feat: ignore button (#48)

--- src/app/translations.ts
+++ src/app/translations.ts
@@ -32,6 +32,7 @@ export const de = {
     arrowDownIconAlt: 'Erklärung öffnen',
     videoIconAlt: 'Ein Erklärungsvideo ist verfügbar',
     arrowRightAlt: 'Erklärungen schliessen',
+    ignore: 'Ignorieren',
 
     //settings
     settingsIconAlt: 'Witty Dashboard Einstellungen öffnen',
@@ -81,6 +82,7 @@ export const en = {
     arrowDownIconAlt: 'Open explanation',
     videoIconAlt: 'A video is available for more learning',
     arrowRightAlt: 'Close explanation',
+    ignore: 'Ignore',
 
     //settings
     settingsIconAlt: 'Open Witty Dashboard settings',
