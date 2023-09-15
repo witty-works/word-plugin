d2c9ae5ee0aff7420d3986967b972a6f61e9f0f1
Solveig Helland <45122311+hellasol@users.noreply.github.com>
2023-09-15 15:45:28 +0200

    fix: typerror (#47)

--- src/app/spellchecker/spellchecker.component.ts
+++ src/app/spellchecker/spellchecker.component.ts
@@ -28,7 +28,7 @@ export class SpellcheckerComponent implements OnInit {
 
   isLoggedin = false;
   
-  lang = Office.context.displayLanguage?.split('-')[0].toLowerCase() === 'de' ? de : en;
+  lang = Office.context?.displayLanguage?.split('-')[0].toLowerCase() === 'de' ? de : en;
 
   paragraphs: string[] = [];
 
