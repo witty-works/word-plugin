f30756fff68c5d81349d2307992606485e25663e
Lukas Kahwe Smith <lukas.smith@witty.works>
2023-11-10 15:21:45 +0100

    bump version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  package_version: "1.2.4",
+  package_version: "1.2.5",
   production: true,
   environment: "production",
   sentry_dsn: "https://1b99be5ef9f8413614d3183638b22663@o512991.ingest.sentry.io/4505624728502272",
