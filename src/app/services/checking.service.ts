af03bcd62cac323a82bd8f9f84cce9311f467033
Lukas Kahwe Smith <lukas.smith@witty.works>
2023-11-21 17:28:52 +0100

    5001 error (#100)
    * fix: deal with not logged in users
    
    * fix: catch 5001 error
    
    ---------
    
    Co-authored-by: Solveig Helland <hi@solveig.io>

--- src/app/services/checking.service.ts
+++ src/app/services/checking.service.ts
@@ -50,7 +50,7 @@ export class CheckingService {
         });
     } catch (error: any) {
       console.log(error);
-      if (error.code === 13001 || error.code === 13002 || error.code === 13000) {
+      if (error.code === 13001 || error.code === 13002 || error.code === 13000 || error.code === 5001) {
         const message = document.getElementById("warn-not-signed-in-word");
         if (message) {
             message.style.display = 'block';
--- src/app/spellchecker/spellchecker.component.ts
+++ src/app/spellchecker/spellchecker.component.ts
@@ -63,7 +63,7 @@ export class SpellcheckerComponent implements OnInit {
 
   register() {
     this.authService.makeAuthRequest().then((response) => {
-      if (response.code === 13001 || response.code === 13002 || response.code === 13000) { //not logged in word, did not consent to add-in permissions
+      if (response.code === 13001 || response.code === 13002 || response.code === 13000 || response.code === 5001) { //not logged in word, did not consent to add-in permissions
         this.isLoggedInWord = false;
         this.isLoggedin = false;
         localStorage.setItem('is_logged_in', 'false');
@@ -77,10 +77,6 @@ export class SpellcheckerComponent implements OnInit {
       if(response.code === 13013) { //edge case: throttled
         this.isLoggedin = false;
         localStorage.setItem('is_logged_in', 'false');
-        return;
-      }
-      if(response.code === 13013) { //edge case: throttled
-        this.isLoggedin = false;
         return;
       }
       console.log('ESCAPED', response)
