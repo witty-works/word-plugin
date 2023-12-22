ca2f7f0cfad5c2f41a49593d324cb74e56f0315a
Lukas Kahwe Smith <lukas.smith@witty.works>
2023-12-22 13:54:59 +0100

    update NLP API dev URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,7 +11,7 @@ export const environment = {
   sentry_sample_rate: 0.0,
   sentry_traces_sample_rate: 0.0,
   sentry_package_name: "word-plugin",
-  api: "https://dev-54ta5gq-2ycn7tlsdvd2o.fr-4.platformsh.site/",
+  api: "https://dev-54ta5gq-jyeciedibdzvq.fr-4.platformsh.site/",
   dashboard: "https://dev-54ta5gq-56xlfiudba6c2.fr-4.platformsh.site/",
   plugin: "https://localhost:4200/word-plugin/",
 };
