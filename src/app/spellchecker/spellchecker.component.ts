d97f8702bdcdc0706869e8fa5566d976252f2807
Solveig Helland <hi@solveig.io>
2024-06-21 17:02:06 +0200

    fix: show message when no text selected

--- src/app/spellchecker/spellchecker.component.ts
+++ src/app/spellchecker/spellchecker.component.ts
@@ -233,7 +233,8 @@ export class SpellcheckerComponent implements OnInit {
             const body = context.document.body;
             body.load('text');
             await context.sync();
-            this.selectedText = body.text.substring(0, maxTextLength);
+            this.selectedText = body.text.substring(0, maxTextLength); 
+            this.hitMaxTextLength = true;
           } else if (this.selectedText.length > maxTextLength) {
             this.hitMaxTextLength = true;
             const selectedTextWithinRange = this.selectedText.substring(0, maxTextLength);
