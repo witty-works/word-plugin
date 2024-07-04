76fb3e039dbd7fec46ca625827c506ce1fef63db
henih <86572062+henih@users.noreply.github.com>
2024-07-04 16:53:07 +0200

    refactor: Improve 403 error handling and authentication in makeAuthRequest

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -12,9 +12,9 @@ export class AuthService {
 
   constructor(private http: HttpClient, private ErrorUtils: ErrorUtils) { }
 
-  async makeAuthRequest(): Promise<any> {
+async makeAuthRequest(): Promise<any> {
+  let accessTokenWithTimestamp = JSON.parse(localStorage.getItem('word_access_token_with_timestamp') ?? '{}');
     try {
-      let accessTokenWithTimestamp = JSON.parse(localStorage.getItem('word_access_token_with_timestamp') ?? '{}');
 
       if (!accessTokenWithTimestamp?.token || new Date().getTime() - accessTokenWithTimestamp.timestamp > 300000) { //check if token is older than 5 min
         const newAccessToken = await Office.auth.getAccessToken({
@@ -25,7 +25,7 @@ export class AuthService {
         accessTokenWithTimestamp = {
           token: newAccessToken,
           timestamp: new Date().getTime()
-        }
+        };
         localStorage.setItem('word_access_token_with_timestamp', JSON.stringify(accessTokenWithTimestamp));
       }
 
@@ -37,46 +37,45 @@ export class AuthService {
           Authorization: `Bearer ${accessTokenWithTimestamp.token}`,
         }
       };
-      return await this.http.post<any>(url, {}, httpOptions)
-        .toPromise()
-        .catch(error => {
-          if (error.status === 403) {
-            localStorage.removeItem('word_access_token_with_timestamp'); //ensures that we get new token
-            const authFailCounter = localStorage.getItem('authFailCounter') ?? '0';
-            if (parseInt(authFailCounter) <= 2) { //has to be 2 to avoid reaching api limit 
-              const newCounter = parseInt(authFailCounter) + 1;
-              localStorage.setItem('authFailCounter', newCounter.toString());
-              setTimeout(() => {
-                this.makeAuthRequest();
-              }, 1000);
-            } else {
-              const url = `${environment.dashboard}office-register?token=${accessTokenWithTimestamp.token}`;
-              Office.context.ui.displayDialogAsync(url, { height: 80, width: 80 }, function (result) {
-                if (result.status === Office.AsyncResultStatus.Failed) {
-                  console.log('result.error', result.error);
-                }
-              });
-            }
-            throw error;
-          } else {
-            const message = document.getElementById("warn-server-error")
-            if (message) {
-              const lang = Office.context?.displayLanguage?.split('-')[0].toLowerCase() === 'de' ? de : en;
-              ErrorUtils.displayErrorMessage(message, "serverError", lang);
-            }
-          }
-        });
+      // Making the HTTP POST request
+      const response = await this.http.post<any>(url, {}, httpOptions).toPromise();
+      return response;
     } catch (error: any) {
-      const errorMessage = typeof error.message === 'string' ? error.message : JSON.stringify(error, Object.getOwnPropertyNames(error));
-
-      Sentry.captureException(new Error(`Error in makeAuthRequest: ${errorMessage}`));
-      const errorCodes = [13001, 13002, 13000, 5001];
-      if (errorCodes.includes(error?.code)) {
-        const message = document.getElementById("warn-not-signed-in-word")
-        if (message) {
-          const lang = Office.context?.displayLanguage?.split('-')[0].toLowerCase() === 'de' ? de : en;
-          ErrorUtils.displayErrorMessage(message, "notSignedInWarning", lang);
+      if (error.status === 403) {
+        console.log(`403 error in makeAuthRequest: ${error.message}`);        
+        localStorage.removeItem('word_access_token_with_timestamp'); //ensures that we get new token
+        const authFailCounter = localStorage.getItem('authFailCounter') ?? '0';
+        if (parseInt(authFailCounter) <= 2) { //has to be 2 to avoid reaching api limit 
+          const newCounter = parseInt(authFailCounter) + 1;
+          localStorage.setItem('authFailCounter', newCounter.toString());
+          setTimeout(() => {
+            this.makeAuthRequest();
+          }, 1000);
+        } else {
+          const url = `${environment.dashboard}office-register?token=${accessTokenWithTimestamp.token}`;
+          Office.context.ui.displayDialogAsync(url, { height: 80, width: 80 }, function (result) {
+            if (result.status === Office.AsyncResultStatus.Failed) {
+              console.log('result.error', result.error);
+            }
+          });
+        }
+      } else {
+        const errorMessage = typeof error.message === 'string' ? error.message : JSON.stringify(error, Object.getOwnPropertyNames(error));
+        Sentry.captureException(new Error(`Error in makeAuthRequest: ${errorMessage}`));
+        const errorCodes = [13001, 13002, 13000, 5001];
+        if (errorCodes.includes(error?.code)) {
+          const message = document.getElementById("warn-not-signed-in-word");
+          if (message) {
+            const lang = Office.context?.displayLanguage?.split('-')[0].toLowerCase() === 'de' ? de : en;
+            ErrorUtils.displayErrorMessage(message, "notSignedInWarning", lang);
           }
+        } else {
+          const message = document.getElementById("warn-server-error");
+          if (message) {
+            const lang = Office.context?.displayLanguage?.split('-')[0].toLowerCase() === 'de' ? de : en;
+            ErrorUtils.displayErrorMessage(message, "serverError", lang);
+          }
+        }
       }
       return error;
     }
