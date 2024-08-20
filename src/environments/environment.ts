3385be1ed24c4e1cdb281c4a08188c517ee734e8
Lukas Kahwe Smith <lukas.smith@witty.works>
2024-08-20 17:05:11 +0200

    bump version

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  package_version: "1.3.30-dev",
+  package_version: "1.3.31-dev",
   production: false,
   environment: "development",
   sentry_dsn: "",
