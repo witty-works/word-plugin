70fc126a595771a17632a704756b777abca7386c
henih <86572062+henih@users.noreply.github.com>
2024-11-13 11:35:41 +0100

    check if all chunks result in error and display error message

--- src/app/translations.ts
+++ src/app/translations.ts
@@ -38,6 +38,7 @@ export const de = {
   howToSelectText:
     "Wähle einen Textabschnitt zum Prüfen aus. Alternativ werden die ersten 2000 Zeichen geprüft.",
   noIssue: "Glückwunsch, keine Sprachprobleme im Bereich Inklusion gefunden.",
+  cantIdentify: "Sprache des Textes konnte nicht identifiziert werden.",
 
   //error
   tryInstead: "Wie wär's mit ...",
@@ -77,7 +78,7 @@ export const de = {
     "Es gab ein Problem bei der Überprüfung des Textes. Bitte versuche es in ein paar Minuten noch einmal.",
   notSupportedAccount: "Dein Konto wird von Microsoft nicht unterstützt. Bitte kontaktiere: ",
   trialExpired: "Dein Witty Test ist abgelaufen. Um Witty weiterhin nutzen zu können, musst du auf ein kostenpflichtiges Abonnement umsteigen.",
-      
+
   //accessibility
   openNewWindow: "Öffnet in einem neuen Fenster",
   loading: "Lädt...",
@@ -124,6 +125,7 @@ export const en = {
   howToSelectText:
     "Select text to choose a text portion to check. Alternatively the first 2000 characters will be checked.",
   noIssue: "Congratulations, no inclusive language issues found.",
+  cantIdentify: "Language of the text could not be identified.",
 
   //error
   tryInstead: "Try instead ...",
@@ -210,6 +212,7 @@ export const fr = {
   howToSelectText:
     "Sélectionnez du texte pour choisir quelle partie sera analysée. Autrement, les 2000 premiers caractères seront analysés.",
   noIssue: "Félicitations, votre texte est déjà inclusif !",
+  cantIdentify: "La langue du texte n'a pas pu être identifiée.",
 
   //error
   tryInstead: "Essayez plutôt...",
