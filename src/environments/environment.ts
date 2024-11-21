3e82e3f9165d39d008ab22a471953b888a187e24
Lukas Kahwe Smith <lukas.smith@witty.works>
2024-11-21 13:34:44 +0100

    re-add max length check, highlight checked text immediately if limit is reached, increase max length to 10k

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,8 +14,7 @@ export const environment = {
   api: "https://dev-54ta5gq-jyeciedibdzvq.fr-4.platformsh.site/",
   dashboard: "https://dev-54ta5gq-56xlfiudba6c2.fr-4.platformsh.site/",
   plugin: "https://localhost:4200/word-plugin/",
-  maxChunkSize: 500,
-  maxTextLength: 2000,
+  maxTextLength: 10000,
   delayDuration: 300,
   tokenLifetime: 300000, // 5 minutes 
   tokenBuffer: 20000, // 20 seconds
